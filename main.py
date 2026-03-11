9d9c019eae15b4a0f48009bca485e429be8fc04b
ecocu1545 <rustemovelcn@gmail.com>
2026-03-12 01:56:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class MainApp(App):
+    def build(self):
+        return Label(text='Hava Praqnozu Yuklenir...')
+if __name__ == "__main__":
+    MainApp().run()
 
